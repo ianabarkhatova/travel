742700289896495194c3786521306896e3c3ef3d
Iana Barkhatova <yanabarhat@gmail.com>
2024-06-15 13:19:03 +0300

    Finish tentatively Holiday section + responsiveness

--- src/layout/sections/holiday/Holiday.tsx
+++ src/layout/sections/holiday/Holiday.tsx
@@ -3,49 +3,65 @@ import image from "../../../assets/images/favourite/favourite-4.webp";
 import styled from "styled-components";
 import image1 from "../../../assets/images/favourite/heading.png"
 import image2 from "../../../assets/images/favourite/line.png"
-
+import {theme} from "../../../styles/Theme";
 
 
 export const Holiday = () => {
     return (
         <StyledHoliday>
-            <StyledImages>
-                <StyledImageOne src={image}/>
-                <StyledImageTwo src={image1}/>
-            </StyledImages>
+                    <ImageWrapper>
+                        <StyledImage src={image1}/>
+                    </ImageWrapper>
         </StyledHoliday>
     );
 };
 
 const StyledHoliday = styled.section`
-`
-
-const StyledImageOne = styled.img`
-    width: 100%;
-`
-
-const StyledImageTwo = styled.img`
-    width: 560px;
-    height: 130px;
-    position: absolute;
-    top: 52%;
-    left: 10%;
-`
-
-const StyledImages = styled.div`
-    display: flex;
     position: relative;
-    min-width: 600px;
+    padding-top: 0;
+    background-image: url(${image});
+    min-height: 600px;
+`
 
+const ImageWrapper = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 10%;
+    
+    max-width: 560px;
+    width: 100%;
+    max-height: 130px;
+    height: 100%;
+    transform: translateY(-20%);
+
+    @media ${theme.media.medium} {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    
     &::after {
         content: "";
         display: inline-block;
         width: 312px;
         height: 40px;
         background-image: url(${image2});
+        background-size: cover;
+        background-position: center;
         position: absolute;
-        left: 32%;
-        bottom: 10%;
+        left: 50%;
+        bottom: calc(-22% + 1vw);
     }
 `
+
+const StyledImage = styled.img`
+    width: 100%;
+    height: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 0 15px;
+`
+
+
 
